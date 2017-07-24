d184ed11c5f35106269900a8da4d7ef197a6e31f
Omar Ezzine <omarezzine39@gmail.com>
2017-07-24 16:57:38 +0200

    Making the home component

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { AngularFireModule } from 'angularfire2';
+import {FlashMessagesService} from 'angular2-flash-messages';
+import {NavbarComponent} from 'app/components/navbar/navbar.component';
+import * as firebase from 'firebase/app';
+
+
 
 @Component({
   selector: 'app-home',
@@ -7,9 +14,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  loggedin:Boolean;
+
+  constructor(
+    public afAuth: AngularFireAuth,
+    public flashMessage: FlashMessagesService,
+    public navbarComponent: NavbarComponent,
+  
+  ) { }
 
   ngOnInit() {
+  }
+
+  login(){
+    this.navbarComponent.login();
   }
 
 }
